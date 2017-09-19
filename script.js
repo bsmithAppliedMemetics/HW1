fc55faae97426b0fd80850c460c1ff37710fd380
bsmithAppliedMemetics <b.smith@AppliedMemeticsllc.com>
2017-09-18 21:49:37 -0400

    init
    initialization

---
+++ script.js
@@ -0,0 +1,13 @@
+// Madlibs!
+
+// Step 1: Construct an array of company names, like Uber, Lyft, Google, Apple
+
+// Step 2: Construct a silly array of animals
+
+// Step 3: Make a loop that makes a sentence similar to 
+// "My company is an Uber for dogs!" Spit out at least 5 sentences to the console.log()
+
+// Bonus challenge: randomize the selection of the company names and animals
+
+// Bonus challenge for those of you who know some jQuery:
+// populate the HTML with a name of one company!
