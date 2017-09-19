cec1c533dbe727fffb6d4672d72738155bfc4256
bsmithAppliedMemetics <b.smith@AppliedMemeticsllc.com>
2017-09-18 22:38:54 -0400

    Company Names and Animal Names
    add implementation for FE and more animals. Look into if Math.floor was the correct function.

--- script.js
+++ script.js
@@ -11,3 +11,10 @@
 
 // Bonus challenge for those of you who know some jQuery:
 // populate the HTML with a name of one company!
+var comNames = ['IKEA', 'IBM','Porsche','Moët et Chandon','Audi','Burberry','Ralph Lauren Corporation','Deere & Company','Louis Vuitton','Wells Fargo','Adidas','Bank of America','Smirnoff','Hyundai','Johnson & Johnson','Sprite','Heineken Brewery','Prada','Google','BMW','Avon','PepsiCo','Starbucks','Samsung Group',"McDonald's"];
+var animalNames = ['lizards', 'deers', 'chipmunks', 'chimpanzees', 'hogs', 'dogs', 'cats', 'eagles'];
+
+for(var i = 0; i < 5; i++)
+{
+	console.log(comNames[Math.floor(Math.random()*25)] + ' is a company that makes sweaters for ' + animalNames[(Math.floor(Math.random()*8))]);
+}
